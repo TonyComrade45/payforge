6a5fc1ac8f89fab11b9e67985329a81e1fd33b15
TonyComrade45 <TonyComrade45@users.noreply.github.com>
2026-09-04 22:37:15 +0530

    Added The MerchantPayment feature

---
+++ backend/src/main/java/com/payforge/dto/response/PaymentResponse.java
@@ -0,0 +1,17 @@
+package com.payforge.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponse {
+
+    private String message;
+    private String referenceId;
+    private BigDecimal amount;
+}
